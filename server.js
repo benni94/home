75b27ceb0b98910534f91f06aba0f88d2a533840
benni94 <littlecartmen@gmail.com>
2021-09-18 21:33:33 +0200

    add heroke start in package.json

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const express = require('express')
+/* const express = require('express')
 const cors = require('cors')
 const app = express()
 
-app.options('*', cors())
+app.options('*', cors()) */
 
 /* let whitelist = ['http://localhost:4200', 'https://github*','https://eu-api.coolkit.cc:8080/api/user/login']
 app.use(cors({
@@ -17,3 +17,22 @@ app.use(cors({
     return callback(null, true);
   }
 })); */
+
+//Install express server
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.options('*', cors())
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/home'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/home/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 8080);
